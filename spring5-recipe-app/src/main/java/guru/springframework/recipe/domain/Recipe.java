825e2392ce8ce1592b3b7d431ed15df453357594
popcornw <46597178+popcornw@users.noreply.github.com>
2020-08-08 23:05:08 +0100

    jpa mapping between Recipe and Category with @manytomany

--- spring5-recipe-app/src/main/java/guru/springframework/recipe/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -9,13 +9,17 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
 public class Recipe {
 
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -39,7 +43,13 @@ public class Recipe {
 	
 	@Enumerated(value=EnumType.STRING)
 	private Difficulty difficulty;
-
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category",
+	joinColumns = @JoinColumn( name ="recipe_id"), inverseJoinColumns = @JoinColumn(name="category_id"))
+	private Set<Category> categories;
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -135,5 +145,13 @@ public class Recipe {
 	public void setDifficulty(Difficulty difficulty) {
 		this.difficulty = difficulty;
 	}
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
+
 
 }
